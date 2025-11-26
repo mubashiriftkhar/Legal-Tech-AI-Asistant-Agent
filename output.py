1b327925ee659f4e3a46870a61aefaddec4f2439
mubashiriftkhar <102500757+mubashiriftkhar@users.noreply.github.com>
2025-11-26 21:13:36 +0500

    Update output.py

--- output.py
+++ output.py
@@ -23,19 +23,19 @@ This is the User's query: "{query}"
 These are the Retrieved Documents:
 {retrieved_docs}
 
-Step 1: 
-  Analyze the Retrieved Documents and give answer to the User's Query from only retrieved Documents.
-  Explain the answer according to User's Query.
-Step 2:
-   After Consing the Results convert the answer into inner html Format.
-   Format the result in Inner HTML using only tags like <h1> to <h6> ,<p>, <strong>, <b>, <ul>, <li> etc., without <html> or <body> tags.
+ 
+   1. Analyze the Retrieved Documents and Summarize answer to the User's Query from only retrieved Documents.
+   2. After Consing the Results convert the answer into inner html Format.
+   3. Format the result in Inner HTML using only tags like <h1> to <h6> ,<p>, <strong>, <b>, <ul>, <li> etc., without <html> or <body> tags.
+   4. Do not add Explanation or anything before answer.
+   5. Do not start with html``` and end with ``` just generate response in required inline html format.
 """
 
    
 
     # Call OpenAI Chat Completion
     completion = openAIClient.chat.completions.create(
-        model="openai/gpt-oss-20b:free",
+        model="openrouter/bert-nebulon-alpha",
         messages=[
             {"role": "system", "content": "You are a helpful legal assistant that answers queries using retrieved documents."},
             {"role": "user", "content": prompt}
